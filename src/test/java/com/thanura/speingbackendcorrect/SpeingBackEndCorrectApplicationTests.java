2e45afa8de93422fa94069813fc5b498ac92eebe
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-28 17:45:11 +0530

    Some missing files commit

---
+++ src/test/java/com/thanura/speingbackendcorrect/SpeingBackEndCorrectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanura.speingbackendcorrect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeingBackEndCorrectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
