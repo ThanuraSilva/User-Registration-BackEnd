920d9172ec6e56c68bfa04bd101df9bd51a80500
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-28 16:00:18 +0530

    UserController create

---
+++ src/main/java/com/thanura/api/UserController.java
@@ -0,0 +1,52 @@
+package com.thanura.api;
+import com.thanura.security.JWTUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import com.thanura.service.UserService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private  final JWTUtils jwtUtils;
+
+    @GetMapping(path="/users/test")
+    public String testing(){
+        return "IJSE";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path="/users/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PostMapping(path = "/users/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> loginUser(@RequestBody User loginCredentials) {
+        try {
+            UserDetails userDetails = userService.loadUserByUsername(loginCredentials.getUsername());
+            if (userDetails.getPassword().replace("{noop}", "").equals(loginCredentials.getPassword())){
+               String jwt = jwtUtils.generateJwtToken(userDetails.getUsername());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+//                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                return new ResponseEntity<>(jwt, HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Invalid login credentials", HttpStatus.UNAUTHORIZED);
+        }catch (UsernameNotFoundException e){
+            return new ResponseEntity<>("User does not exist", HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
+
